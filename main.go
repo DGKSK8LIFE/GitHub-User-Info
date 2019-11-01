587c66da0563fa914a284cecc9897f0ff53fe2b8
Tarek Ali <tarekali15@outlook.com>
2019-11-01 09:27:23 -0500

    shows json on the website, need to format it

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -14,7 +16,16 @@ func init() {
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		username := r.FormValue("username")
+		uInfo, err := http.Get("https://api.github.com/users/" + username)
+		stringUinfo, err := ioutil.ReadAll(uInfo.Body)
+		if err != nil {
+			panic(err)
+		}
 		html.ExecuteTemplate(w, "main.html", nil)
+		if len(username) != 0 {
+			fmt.Fprintln(w, string(stringUinfo))
+		}
+
 	})
 	http.ListenAndServe(":8080", nil)
 }
