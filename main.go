969da4d08cb8443ef4864a9139bf8126eda8fd50
Tarek Ali <tarekali15@outlook.com>
2019-11-01 09:54:00 -0500

    integrating json things

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
-	"io/ioutil"
 	"net/http"
 )
+
+type UserInfo struct {
+	Name        string
+	Login       string
+	ID          int
+	Company     string
+	Blog        string
+	Location    string
+	Email       string
+	Hireable    bool
+	Bio         string
+	PublicRepos string
+	PublicGists string
+	Followers   int
+	Following   int
+}
 
 var html *template.Template
 
@@ -16,16 +32,25 @@ func init() {
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		username := r.FormValue("username")
-		uInfo, err := http.Get("https://api.github.com/users/" + username)
-		stringUinfo, err := ioutil.ReadAll(uInfo.Body)
-		if err != nil {
-			panic(err)
-		}
+		user := &UserInfo{}
+		userJSON := getJSON("https://api.github.com/users/"+username, user)
 		html.ExecuteTemplate(w, "main.html", nil)
 		if len(username) != 0 {
-			fmt.Fprintln(w, string(stringUinfo))
+			fmt.Fprintf(w, "Info: %v", userJSON)
 		}
 
 	})
 	http.ListenAndServe(":8080", nil)
+}
+
+var site = &http.Client{}
+
+func getJSON(url string, target interface{}) error {
+	r, err := site.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
 }
